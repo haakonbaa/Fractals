49008e4bcfda1f52b47d14df6e2b1e0ceeb0bf0c
Håkon Bårsaune <haakbaa@stud.ntnu.no>
2022-04-29 19:16:21 +0200

    first image

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -20,23 +19,36 @@ func mandelbrotIters(c complex128) uint {
 
 func mapCmplx(x int, y int, dims image.Point, tl complex128, br complex128) complex128 {
 	return complex(
-		float64(x-dims.X)/float64(dims.X)*real(br-tl) + real(tl),
-		float64(y-dims.Y)/float64(dims.Y)*imag(br-tl) + imag(tl))
+		float64(x)/float64(dims.X-1)*real(br-tl) + real(tl),
+		float64(y)/float64(dims.Y-1)*imag(br-tl) + imag(tl))
+}
+
+func Max( x, y uint ) uint {
+	if x > y {
+		return x
+	}
+	return y
 }
 
 func main() {
 	upLeft := image.Point{0,0}
-	width := 200
-	height := 200
+	width := 2000
+	height := 2000
 	lowRight := image.Point{width,height}
 	img := image.NewRGBA(image.Rectangle{upLeft,lowRight})
+	var largest uint = 0
 	for x:= 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			img.Set(x, y, color.RGBA{100,200,200,0xff})
+			var c complex128 = mapCmplx(x, y, lowRight,complex(-2,1.3),complex(0.6,-1.3))
+			iters := mandelbrotIters(c)
+			largest = Max(iters,largest)
+			if iters == 200 {
+				img.Set(x, y, color.RGBA{0,0,0,0xff})
+			} else {
+				img.Set(x, y, color.RGBA{100,200,200,0xff})
+			}
 		}
 	}
-	fmt.Println("Hello World!")
-
 	f, _ := os.Create("image.png")
 	png.Encode(f,img)
 }
